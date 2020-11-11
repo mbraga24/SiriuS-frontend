765cb6258ba0aa02df9c7cbd3f4bf0e6ac447119
github username <rdg.brg@gmail.com>
2020-11-11 16:18:15 -0500

    Change invitation form

--- src/components/InvitationForm.js
+++ src/components/InvitationForm.js
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { Form } from 'semantic-ui-react';
+import { Form, Checkbox, Divider, Transition } from 'semantic-ui-react';
 import useFormFields from '../hooks/useFormFields'
 import '../resources/InvitationForm.css';
 
 const InvitationForm = props => {
   
   const keyHolder = useSelector(state => state.app.keyHolder)
-  const [ radioCheck, setRadioCheck ] = useState(null)
+  const [ toggleCheck, setToggleCheck ] = useState(false)
   const [ checkbox, setCheckbox ] = useState(false)
-  const [ disableField, setDisableField ] = useState(false)
   const [ fields, handleFieldChange ] = useFormFields({
     email: "",
     firstName: "",
@@ -18,11 +17,8 @@ const InvitationForm = props => {
     customInvitation: "",
   })
 
-  const companyFielLabel = `If user does not work at ${keyHolder.company} and is not self-employed please provide the name of the company: `
-
-  const handleChange = (e, { value }) => {
-    value === "yes" || value === "self" ? setDisableField(true) : setDisableField(false)
-    setRadioCheck(value)
+  const handleToggle = (e, { value }) => {
+    setToggleCheck(!toggleCheck)
   }
 
   const handleInvitation = () => {
@@ -30,72 +26,51 @@ const InvitationForm = props => {
 
     const data = {
       email: fields.email,
-      current_user: keyHolder.id,
+      current_user_id: keyHolder.id,
       first_name: fields.firstName,
       last_name: fields.lastName,
       company: keyHolder.company,
       custom_invitation: fields.customInvitation
     }
     
-    fetch(`http://localhost:3000/invites/`, {
+    fetch('http://localhost:3000/invites/', {
+      mode: "cors",
       method: "POST",
       headers: {
+        'Access-Control-Allow-Origin':'*',
         "Content-Type": "application/json"
       },
       body: JSON.stringify(data)
     })
-    .then(r => r.json())
-    .then(invitation => {
-      console.log(invitation)
-    })
+    .then(r => r.json()).then(console.log)
 
     props.setSecondOpen(true)
   }
 
-  // /send-invitation
-
-  console.log("CHECKBOX -->", checkbox)
-  console.log("RADIOCHECK -->", radioCheck)
+  console.log("TOGGLECHECK -->", toggleCheck)
 
   return (
-    <Form onSubmit={handleInvitation}>
+    <Form id="Invitation-Form" onSubmit={handleInvitation}>
       <Form.Group widths='3'>
         <Form.Input fluid name="email" label='Email' placeholder='Email' onChange={handleFieldChange} />
-      </Form.Group>
-      <Form.Group widths='4'>
         <Form.Input fluid name="firstName" label='First name' placeholder='First name' onChange={handleFieldChange} />
         <Form.Input fluid name="lastName" label='Last name' placeholder='Last name' onChange={handleFieldChange} />
       </Form.Group>
-      <Form.Group inline>
-        <label>Collaborator also works at {keyHolder.company}</label>
-        <Form.Radio
-          label='Yes'
-          value='yes'
-          checked={radioCheck === 'yes'}
-          onChange={handleChange}
-        />
-        <Form.Radio
-          label='No'
-          value='no'
-          checked={radioCheck === 'no'}
-          onChange={handleChange}
-        />
-        <Form.Radio
-          label='Self-employed'
-          value='self'
-          checked={radioCheck === 'self'}
-          onChange={handleChange}
-        />
+      <Form.Group>
+        <label className="Checkbox-Label">Write a custom invitation</label>
+        <Checkbox toggle onChange={handleToggle} checked={toggleCheck} className="Checkbox-Item" />
       </Form.Group>
-      <Form.Group widths='1'>
-        <Form.Input 
-          name="companyName"
-          label={companyFielLabel} 
-          placeholder='Company Name'
-          className={disableField ? "disabled" : null} 
-          onChange={handleFieldChange} />
-      </Form.Group>
-      <Form.TextArea name="customInvitation" label='Write a custom invitation ' placeholder='Come join us!' onChange={handleFieldChange} />
+      <div>
+        <Transition visible={toggleCheck} animation='fade down' duration={500}>
+          <Form.TextArea 
+          className={toggleCheck ? "TextArea-Field" : "Hide-Field"}
+          name="customInvitation" 
+          placeholder='Come join us!' 
+          onChange={handleFieldChange} 
+          />
+        </Transition>
+      </div>
+      <Divider hidden />
       <Form.Checkbox onClick={() => setCheckbox(!checkbox)} label='Alert me when this user finishes signing up' />
       <Form.Group className="Invitation-Form-Submit-Btn-Wrapper">
         <Form.Button>Send Invitation</Form.Button>
