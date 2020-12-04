b195456f32de36bab7a3ab855c9144cc1ccee43d
github username <rdg.brg@gmail.com>
2020-12-04 17:07:07 -0500

    Extracted RelaunchModal styles from component

---
+++ src/helpers/RelaunchModalsStyles.js
@@ -0,0 +1,23 @@
+export const styleBtn = {
+  backgroundColor: "#534292",
+  color: "#ffffff"
+}
+
+export const styleBtnLoad = {
+  backgroundColor: "#534292",
+  width: "165px",
+  color: "#ffffff"
+}
+
+export const styleIcon = {
+  color: "#79589f"
+}
+
+export const styleDeleteBtn = {
+  backgroundColor: "#ac4444",
+  color: "#ffffff"
+}
+
+export const styleMessageDisplay = {
+  display: "block"
+}
