5dfe1ab51e5ab82c395bd212a0c5c393639168f9
github username <rdg.brg@gmail.com>
2020-12-11 17:07:38 -0500

    Artist attribution

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { withRouter } from 'react-router-dom';
 import Signup from "./Signup";
-import { Container, Grid, Image } from 'semantic-ui-react';
+import { Container, Grid, Image, Popup } from 'semantic-ui-react';
 import '../resources/Home.css';
 import '../resources/Signup.css';
 
 const Home = props => {
+
+  const popupStyle = {
+    borderRadius: 4,
+    padding: '0.5em'
+  }
+
+
 
   return(
     <Container id="Home-Container">
@@ -15,12 +22,21 @@ const Home = props => {
               <Grid columns="equal" stackable >
                 <Grid.Row>
                   <Grid.Column textAlign="center">
-                    <Image src={require('../Icon/polygonal-crab-inverted.png')} size='big' />
+                    <Popup style={popupStyle} inverted position='top center' on="click" pinned trigger={<Image src={require('../Icon/polygonal-crab-inverted.png')} />}>
+                      Icon made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+                    </Popup>
                   </Grid.Column>
                   <Grid.Column className="Home-Call-Action-Wrapper" textAlign="center">
-                    <p className="Home-Call-Action-1">
-                      Join our community in a new and slacking proof way to work and let's get <span>SiriuS!</span>
-                    </p>
+                  <Popup 
+                    inverted 
+                    position='bottom center' 
+                    content={"Click the icon for the artist's attribution."} 
+                    style={popupStyle} 
+                    trigger={
+                      <p className="Home-Call-Action-1">
+                        Join our community in a new and slacking proof way to work and let's get <span>SiriuS!</span>
+                      </p>
+                    } />
                   </Grid.Column>
                 </Grid.Row>
                 <Grid.Row>
