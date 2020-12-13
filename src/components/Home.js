a6853feb95eb6e924a5ce4d993458ce9fa9d79f3
github username <rdg.brg@gmail.com>
2020-12-13 16:56:30 -0500

    Change Home page design

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,38 +18,40 @@ const Home = props => {
     <Container id="Home-Container">
       <Grid stackable>
         <Grid.Row>
-          <Grid.Column className="Logo-Action-Wrapper" width={10}>
-              <Grid columns="equal" stackable >
-                <Grid.Row>
-                  <Grid.Column textAlign="center">
-                    <Popup style={popupStyle} inverted position='top center' on="click" pinned trigger={<Image src={require('../Icon/polygonal-crab-inverted.png')} />}>
-                      Icon made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
-                    </Popup>
-                  </Grid.Column>
-                  <Grid.Column className="Home-Call-Action-Wrapper" textAlign="center">
+          <Grid.Column className="Logo-Action-Wrapper" width={8}>
+            <Grid columns="equal" stackable >
+              <Grid.Row>
+                <Grid.Column className="Image-Wrapper">
+                  <Popup style={popupStyle} inverted position='top center' on="click" pinned trigger={<Image src={require('../Icon/polygonal-crab-inverted.png')}/>}>
+                    Icon made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+                  </Popup>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column textAlign="center">
                   <Popup 
                     inverted 
                     position='bottom center' 
                     content={"Click the icon for the artist's attribution."} 
                     style={popupStyle} 
                     trigger={
-                      <p className="Home-Call-Action-1">
-                        Join our community in a new and slacking proof way to work and let's get <span>SiriuS!</span>
+                      <p className="Call-Action">
+                        Create your new team, set goals, keep track of your progress, share your ideas, archive your work and more.
                       </p>
                     } />
-                  </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>
-                  <Grid.Column textAlign="center">
-                    <p className="Home-Call-Action-2">
-                      Create your new team, set goals, keep track of your progress, share your ideas, archive your work and more. 
-                    </p>
-                  </Grid.Column>
-                </Grid.Row>
-              </Grid>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
           </Grid.Column>
-          <Grid.Column width={6}>    
+          <Grid.Column width={8}>    
             <Signup />
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column textAlign="center" width={16}>    
+            <p className="Call-Action Bottom">
+              Join our community in a new way to work and collaborate. Let's get <span>SiriuS!</span>
+            </p>
           </Grid.Column>
         </Grid.Row>
       </Grid>
